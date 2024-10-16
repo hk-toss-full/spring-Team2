83eeb51fe96eaa95e140117217e3faab54c523e5
JoGyoungJun <yoop80075@gmail.com>
2024-10-16 15:44:42 +0900

    first commit

---
+++ WebSocket_QuerryDSL/src/main/java/team2/WebSocket_QuerryDSL/WebSocketQuerryDslApplication.java
@@ -0,0 +1,13 @@
+package team2.WebSocket_QuerryDSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketQuerryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketQuerryDslApplication.class, args);
+	}
+
+}
---
+++ WebSocket_QuerryDSL/src/test/java/team2/WebSocket_QuerryDSL/WebSocketQuerryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.WebSocket_QuerryDSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketQuerryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
